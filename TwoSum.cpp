ca6adcc46906475f4fbb5d417150320b0df369e6
Fish Lin <lucylin0819@gmail.com>
2018-05-02 00:22:13 +0800

    Two Sum

---
+++ TwoSum.cpp
@@ -0,0 +1,91 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		vector<int> result;
+		for (int i = 0; i < nums.size() - 1; i++) {
+			for (int j = i + 1; j < nums.size(); j++) {
+				if (nums[i] + nums[j] == target) {
+					result.push_back(i);
+					result.push_back(j);
+					return result;
+				}
+				else if (nums[i] + nums[j] > target) break;
+			}
+		}
+	}
+};
+
+/* default */
+
+void trimLeftTrailingSpaces(string &input) {
+	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+		return !isspace(ch);
+	}));
+}
+
+void trimRightTrailingSpaces(string &input) {
+	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+		return !isspace(ch);
+	}).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+	vector<int> output;
+	trimLeftTrailingSpaces(input);
+	trimRightTrailingSpaces(input);
+	input = input.substr(1, input.length() - 2);
+	stringstream ss;
+	ss.str(input);
+	string item;
+	char delim = ',';
+	while (getline(ss, item, delim)) {
+		output.push_back(stoi(item));
+	}
+	return output;
+}
+
+int stringToInteger(string input) {
+	return stoi(input);
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+	if (length == -1) {
+		length = list.size();
+	}
+
+	if (length == 0) {
+		return "[]";
+	}
+
+	string result;
+	for(int index = 0; index < length; index++) {
+		int number = list[index];
+		result += to_string(number) + ", ";
+	}
+	return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+	string line;
+	while (getline(cin, line)) {
+		vector<int> nums = stringToIntegerVector(line);
+		getline(cin, line);
+		int target = stringToInteger(line);
+		
+		vector<int> ret = Solution().twoSum(nums, target);
+
+		string out = integerVectorToString(ret);
+		cout << out << endl;
+	}
+	return 0;
+}
